ed81ffe45ce26994eb29684ec8c6f2f1af2c2e78
Marcio Duarte <zmduartez@gmail.com>
2023-07-10 14:00:23 +0000

    Add finetuning code - resnet - clip - vamos

--- notebooks/VAM-OS-2/config.py
+++ notebooks/VAM-OS-2/config.py
@@ -4,11 +4,15 @@ DATASET_CSV_PATH = f"{WORKING_DIR}/dataset.csv"
 SPECTROGRAM_DIR = f"{WORKING_DIR}/spectrograms"
 INPUT_DIR = "/home/admin/FEUP-Synthesizing-Audio-from-Textual-Input/data/input/audio-mnist/data"
 RESULTS_DIR = f"{WORKING_DIR}/results/VAM-OS-2"
+CHECKPOINTS_DIR = f"{RESULTS_DIR}/checkpoints"
+LOG_DIR = f"{RESULTS_DIR}/logs"
 
 # Hyperparameters
-BATCH_SIZE = 512
+BATCH_SIZE = 128
 MAX_INPUT_SIZE = 65536
 
 # Training settings
 LEARNING_RATE = 0.001
-EPOCHS = 80
+EPOCHS = 1000
+EARLY_STOPPING_PATIENCE = 5
+LOAD_CHECKPOINT = False
--- notebooks/VAM-OS-2/res_net/__init__.py
+++ notebooks/VAM-OS-2/res_net/__init__.py
@@ -1,23 +1,35 @@
+from config import LOAD_CHECKPOINT
 from res_net.res_net import ResNet
 from res_net.res_block import ResBlock
 import torch
 from torchsummary import summary
 from data.tools import get_loaders
-from res_net.trainer import fit
+from res_net.trainer import fine_tune, fit
 
 
 def main():
-    print("slkjasldj")
-    # this function is the one custom made to train the resnet
-    resnet18 = ResNet(1, ResBlock, [2, 2, 2, 2],
-                      useBottleneck=False, outputs=10)
-    resnet18.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
-    summary(resnet18, (1, 257, 128))
+    # Define the hyperparameters range for fine-tuning
+    hyperparameters_range = {
+        "kernel_size": [7, 9, 11],
+        "stride": [1, 2, 3],
+        "padding": [1, 2, 3],
+        "pool_kernel_size": [2, 3, 4],
+        "pool_stride": [1, 2],
+        "pool_padding": [0, 1],
+    }
 
+    # Set the number of trials for fine-tuning
+    num_trials = 5
+
+    # Split the data into train and validation loaders
     train_loader, val_loader = get_loaders(train_percentage=0.8)
 
-    # call the fit function
-    fit(resnet18, train_loader, val_loader)
+    # Perform fine-tuning with random search
+    # it receives the class in the first argument
+    fine_tune(ResNet, train_loader, val_loader, hyperparameters_range, num_trials, epochs=5)
+
+    # Uncomment the following line if you want to continue training with the original settings
+    # fit(resnet18, train_loader, val_loader, save_checkpoint=True, load_checkpoint=LOAD_CHECKPOINT)
 
 if __name__ == "__main__":
     main()
